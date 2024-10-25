c8b4e259ba4b44574c487b881aadf3ac0c9d6db2
dominik-pichler <info@dominik-pichler.com>
2024-10-25 08:46:54 +0200

    up

--- src/neo4j_Connector.py
+++ src/neo4j_Connector.py
@@ -1,25 +1,21 @@
 from neo4j import GraphDatabase
 import pandas as pd
 
-# Define your Neo4j connection details
-uri = "bolt://localhost:7687"  # Update with your Neo4j URI
-username = "neo4j"  # Update with your username
-password = "password"  # Update with your password
+uri = "bolt://localhost:7687"  
+username = "neo4j"  
+password = "password"  
 
-# Establish a connection to the Neo4j database
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
 
 def fetch_graph_data():
     with driver.session() as session:
-        # Define your Cypher query to fetch nodes and relationships
         query = """
         MATCH (n)-[r]->(m)
         RETURN n.id AS source, m.id AS target, r.weight AS weight
         """
         result = session.run(query)
 
-        # Convert the result to a pandas DataFrame
         data = pd.DataFrame([dict(record) for record in result])
 
         return data
